6cfe058b356e2dc304a3e1cbb2ac68d3f7368e7f
Panachaifame <panachai.fame@gmail.com>
2024-07-27 11:30:19 +0700

    Practice2_Ch04_ReactHook

--- components/ProfileSrceen.tsx
+++ components/ProfileSrceen.tsx
@@ -1,22 +1,29 @@
-import { StyleSheet, Text, View, Image } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View, Image, Button } from "react-native";
+import React, { useState } from "react";
 import styles from "../styles/styles";
 
 const ProfileSrceen = (): React.JSX.Element => {
+  const [name, setName] = useState("Panachai Lormngkhonkit");
+  const [Img, setImg] = useState(require("../assets/Profile_1.jpg"));
+  const handleChangeName = () => {
+    setName("New Name");
+  };
+  const handleChangeImg = () => {
+    setImg(require("../assets/Profile_2.jpg"));
+  };
   return (
     <View style={styles.container}>
       <View style={styles.profileContainer}>
-        <Image
-          source={require("../assets/Profile_1.jpg")}
-          style={styles.profileImage}
-        />
-        <Text style={styles.profileName}>Panachai Lormongkhonkit</Text>
+        <Image source={Img} style={styles.profileImage} />
+        <View>
+          <Text style={styles.profileName}>{name}</Text>
+          <Button title="CHANGE NAME" onPress={handleChangeName} />
+          <Text></Text>
+          <Button title="CHANGE IMAGE" onPress={handleChangeImg} />
+        </View>
       </View>
     </View>
   );
 };
 
 export default ProfileSrceen;
-
-
-
