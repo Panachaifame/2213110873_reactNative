b4db169b043231767ddb908f88fca3b12056b0a5
Panachaifame <panachai.fame@gmail.com>
2024-08-31 11:59:53 +0700

    Lecture8_StackNavigator

--- App.tsx
+++ App.tsx
@@ -1,23 +1,47 @@
 import { View, Text } from "react-native";
 import React from "react";
+
 import HomeScreen from "./screens/HomeScreen";
 import AboutScreen from "./screens/AboutScreen";
 import CreatePostScreen from "./screens/CreatePostScreen";
 
-import { NativeScreenNavigationContainer } from "react-native-screens";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-const App = (): React.JSX.Element => {
-  const HomeStack = createNativeStackNavigator();
+function App(): React.JSX.Element {
+  const Homestack = createNativeStackNavigator();
+
   return (
     <NavigationContainer>
-      <HomeStack.Navigator>
-        <HomeStack.Screen name="Home" component={HomeScreen} />
-        <HomeStack.Screen name="About" component={AboutScreen} />
-        <HomeStack.Screen name="Create Post" component={CreatePostScreen}/>
-      </HomeStack.Navigator>
+      <Homestack.Navigator 
+      initialRouteName="Home"
+      screenOptions={{
+          headerStyle:{backgroundColor:'#00dea5'},
+          headerTintColor:'white',
+          headerTitleStyle:{fontWeight:'bold'},
+          //headerTitleAlign:'center'
+      }}
+      >
+        <Homestack.Screen 
+        name="Home" 
+        component={HomeScreen} 
+        options={{title:'หน้าหลัก'}}
+        />
+        <Homestack.Screen 
+        name="About" 
+        component={AboutScreen} 
+        /* options={{title :"เกี่ยวกับเรา",
+        headerStyle:{backgroundColor:'#f2f2'},
+        headerTintColor:'white',
+        headerTitleStyle:{fontWeight:'Bold'},
+        headerTitleAlign:'center'
+        }} */
+        
+        />
+        <Homestack.Screen name="CreatePost" component={CreatePostScreen} />
+      </Homestack.Navigator>
     </NavigationContainer>
   );
-};
+}
+
 export default App;
